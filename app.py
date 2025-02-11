af5c88ccf66cff54a53508f9eef1822140376dd9
worthyag <worthydevelops@gmail.com>
2025-02-11 20:05:10 +0000

    Updating sql deets.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import traceback
 app = Flask("MoviesCatalog")
 app.secret_key = "ddj3934u8en"
 
-app.config["MYSQL_HOST"] = "192.168.2.136"
+app.config["MYSQL_HOST"] = "192.168.0.63"
 app.config["MYSQL_USER"] = "flaskuser"
 app.config["MYSQL_PASSWORD"] = "password12-"
 app.config["MYSQL_DB"] = "moviesDB"
